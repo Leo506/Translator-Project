f297724fa31ff12c7ac722c3f844852f9db312d2
Leo506 <64748654+Leo506@users.noreply.github.com>
2020-05-19 23:26:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import PySimpleGUI as sg
 from translate_code import translate
 
-sg.theme("Dark Green 5")
+sg.theme("DarkBlack")
 
 layout_main = [[sg.Text('Welcome to ALP', justification='r', size=(30, 1))],
           [sg.Text('       Choose text file with your Algorithm to translate in Python')],
-          [sg.In(disabled=True), sg.FileBrowse()],
+          [sg.In(disabled=True, text_color='black'), sg.FileBrowse()],
           [sg.Button('Translate')],
-          [sg.Multiline(default_text='Here will be Python code', key='result', size=(70, 10))],
+          [sg.Multiline(default_text='Here will be Python code', key='result', size=(60, 10))],
           [sg.Exit()]
           ]
 
-layout_rules = [[sg.Text('Внимание! Перед использованием программы ознакомтесь с правилами!', justification='c')],
+layout_rules = [[sg.Text('Внимание! Перед использованием программы ознакомтесь с правилами!', justification='c', text_color='red')],
                 [sg.Text('1. Перед запуском переводчика проверьте алгоритм на синтаксические ошибки', justification='l')],
                 [sg.Text('2. В данной версии программы не используются служебные слова "то" и слова, описывающие предназначение кода', justification='l')],
                 [sg.Text('3. Соблюдайте логическую табуляцию (не надо писать длинные инструкции в одной строке)', justification='l')],
@@ -35,7 +35,6 @@ while True:
         with open(values[0], 'r', encoding='utf-8') as file:
             buffer = [i for i in file]
         result_buffer = translate(buffer)
-        print(result_buffer)
         st = ''
         for i in result_buffer:
             st += i
